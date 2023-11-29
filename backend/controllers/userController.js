9be1624d5b6f950a2805c5a28e57f4587ed58526
ajit1028 <ajeets1028@gmail.com>
2023-11-29 13:27:59 +0530

    final commit

---
+++ backend/controllers/userController.js
@@ -0,0 +1,45 @@
+const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
+const crypto = require('crypto');
+
+// Generate a random 256-bit key (32 bytes) for the SECRET environment variable
+const secretKey = crypto.randomBytes(32).toString('hex');
+process.env.SECRET = secretKey;
+
+const createToken = (_id) => {
+  return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d' })
+}
+
+// login a user
+const loginUser = async (req, res) => {
+    const {email, password} = req.body
+  
+    try {
+      const user = await User.login(email, password)
+  
+      // create a token
+      const token = createToken(user._id)
+  
+      res.status(200).json({email, token})
+    } catch (error) {
+      res.status(400).json({error: error.message})
+    }
+  }
+  
+  // signup a user
+  const signupUser = async (req, res) => {
+    const {email, password} = req.body
+  
+    try {
+      const user = await User.signup(email, password)
+  
+      // create a token
+      const token = createToken(user._id)
+  
+      res.status(200).json({email, token})
+    } catch (error) {
+      res.status(400).json({error: error.message})
+    }
+  }
+  
+  module.exports = { signupUser, loginUser }
